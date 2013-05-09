e9a15b34637594d9edf987b27f881812d5664a11
macheretj <macheretj@gmail.com>
2013-05-09 23:57:46 +0200

    Update sps.sh

--- central_node/scripts/sps.sh
+++ central_node/scripts/sps.sh
@@ -2,7 +2,15 @@
 
 #!/bin/bash
 
+echo -e "** Welcome in SPS, a ps command listing only process by strict name"
+echo -e "** Ex: sps.sh ssh-agent, returns only strict ssh-agent process."
+
+if [ $# -eq 0 ]
+then
+    echo -e '** Wrong usage! Please specify a process name!'
+    exit 0
+fi
 
 process_name = $0
 
-while read line; do if [ "`echo $line | awk '{print $8}' | grep -Fx ssh-agent`" == "ssh-agent" ]; then echo $line; fi; done < <(ps -ef | grep ssh-agent)
+while read line; do if [ "`echo $line | awk '{print $8}' | grep -Fx $process_name`" == "$process_name" ]; then echo $line; fi; done < <(ps -ef | grep $process_name)
