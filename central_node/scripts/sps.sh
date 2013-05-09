67d613f5cfe7bd3a83eab7bd00f9b5bf4a581995
macheretj <macheretj@gmail.com>
2013-05-09 23:48:57 +0200

    Update sps.sh

--- central_node/scripts/sps.sh
+++ central_node/scripts/sps.sh
@@ -1,4 +1,8 @@
 # Strict ps -ef by name
 
+#!/bin/bash
+
+
+process_name = $0
 
 while read line; do if [ "`echo $line | awk '{print $8}' | grep -Fx ssh-agent`" == "ssh-agent" ]; then echo $line; fi; done < <(ps -ef | grep ssh-agent)
